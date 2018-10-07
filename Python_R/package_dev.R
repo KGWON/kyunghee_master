1f1dee2c736351859dd13653f34422751d0a8446
GUKWON KU <zxc6125@naver.com>
2018-10-07 13:23:55 +0900

    R 패키지 개발 코드

---
+++ Python_R/package_dev.R
@@ -0,0 +1,26 @@
+if (!require("devtools")) install.packages("devtools")
+if (!require("roxygen2")) install.packages("roxygen2")
+
+## working directory를 설정한다.
+setwd("/Users/ku/Desktop/myproject")
+
+## package directory를 생성한다.
+create("/Users/ku/Desktop/myproject")
+
+## working directory는 유지한 채로 ./R/ 하위에 패키지에 포함할 사용자 정의 함수들을 작성한다.
+
+## document를 생성한다. (help() 함수를 사용했을 때 나오는 document에 관한 것.)
+document()
+
+## 개발한 로컬 패키지를 설치한다.
+install(pkg="R")
+
+## 패키지 로딩 및 테스트
+library(myproject)
+
+Add(1, 2)
+Subtract(1, 2)
+Multiply(1, 2)
+Divide(1, 2)
+?Divide
+?Add
